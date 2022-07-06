d2671c9add72e5df18d91a8a6e19c5b4ab389cc8
Gjermund Bjaanes <gjermund@empower.eco>
2022-07-06 12:22:39 +0200

    Added state info for proof

--- vue/src/store/generated/empowerchain/empowerchain/empowerchain.empowerchain.proofofexistence/module/types/proofofexistence/proof.ts
+++ vue/src/store/generated/empowerchain/empowerchain/empowerchain.empowerchain.proofofexistence/module/types/proofofexistence/proof.ts
@@ -1,18 +1,33 @@
 /* eslint-disable */
+import { Timestamp } from "../google/protobuf/timestamp";
 import { Writer, Reader } from "protobufjs/minimal";
 
 export const protobufPackage = "empowerchain.empowerchain.proofofexistence";
 
 export interface Proof {
+  /** Hash is the SHA-256 hash of the data of which is being made a proof for */
   hash: string;
+  /** Timestamp is the time the proof was added on-chain (block time) */
+  timestamp: Date | undefined;
+  /** Reporter is the account address that created the proof */
+  reporter: string;
 }
 
-const baseProof: object = { hash: "" };
+const baseProof: object = { hash: "", reporter: "" };
 
 export const Proof = {
   encode(message: Proof, writer: Writer = Writer.create()): Writer {
     if (message.hash !== "") {
       writer.uint32(10).string(message.hash);
+    }
+    if (message.timestamp !== undefined) {
+      Timestamp.encode(
+        toTimestamp(message.timestamp),
+        writer.uint32(18).fork()
+      ).ldelim();
+    }
+    if (message.reporter !== "") {
+      writer.uint32(26).string(message.reporter);
     }
     return writer;
   },
@@ -26,6 +41,14 @@ export const Proof = {
       switch (tag >>> 3) {
         case 1:
           message.hash = reader.string();
+          break;
+        case 2:
+          message.timestamp = fromTimestamp(
+            Timestamp.decode(reader, reader.uint32())
+          );
+          break;
+        case 3:
+          message.reporter = reader.string();
           break;
         default:
           reader.skipType(tag & 7);
@@ -42,12 +65,28 @@ export const Proof = {
     } else {
       message.hash = "";
     }
+    if (object.timestamp !== undefined && object.timestamp !== null) {
+      message.timestamp = fromJsonTimestamp(object.timestamp);
+    } else {
+      message.timestamp = undefined;
+    }
+    if (object.reporter !== undefined && object.reporter !== null) {
+      message.reporter = String(object.reporter);
+    } else {
+      message.reporter = "";
+    }
     return message;
   },
 
   toJSON(message: Proof): unknown {
     const obj: any = {};
     message.hash !== undefined && (obj.hash = message.hash);
+    message.timestamp !== undefined &&
+      (obj.timestamp =
+        message.timestamp !== undefined
+          ? message.timestamp.toISOString()
+          : null);
+    message.reporter !== undefined && (obj.reporter = message.reporter);
     return obj;
   },
 
@@ -57,6 +96,16 @@ export const Proof = {
       message.hash = object.hash;
     } else {
       message.hash = "";
+    }
+    if (object.timestamp !== undefined && object.timestamp !== null) {
+      message.timestamp = object.timestamp;
+    } else {
+      message.timestamp = undefined;
+    }
+    if (object.reporter !== undefined && object.reporter !== null) {
+      message.reporter = object.reporter;
+    } else {
+      message.reporter = "";
     }
     return message;
   },
@@ -72,3 +121,25 @@ export type DeepPartial<T> = T extends Builtin
   : T extends {}
   ? { [K in keyof T]?: DeepPartial<T[K]> }
   : Partial<T>;
+
+function toTimestamp(date: Date): Timestamp {
+  const seconds = date.getTime() / 1_000;
+  const nanos = (date.getTime() % 1_000) * 1_000_000;
+  return { seconds, nanos };
+}
+
+function fromTimestamp(t: Timestamp): Date {
+  let millis = t.seconds * 1_000;
+  millis += t.nanos / 1_000_000;
+  return new Date(millis);
+}
+
+function fromJsonTimestamp(o: any): Date {
+  if (o instanceof Date) {
+    return o;
+  } else if (typeof o === "string") {
+    return new Date(o);
+  } else {
+    return fromTimestamp(Timestamp.fromJSON(o));
+  }
+}
