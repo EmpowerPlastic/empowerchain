0e4dd89c687d67d767405188d8e6bca0c1b6c9d9
Gjermund Bjaanes <gjermund@empower.eco>
2022-07-19 16:09:35 +0200

    Merged msg server into single file

--- x/proofofexistence/keeper/msg_server.go
+++ x/proofofexistence/keeper/msg_server.go
@@ -1,6 +1,9 @@
 package keeper
 
 import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/empowerchain/empowerchain/x/proofofexistence/types"
 )
 
@@ -15,3 +18,13 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 }
 
 var _ types.MsgServer = msgServer{}
+
+func (k msgServer) CreateProof(goCtx context.Context, msg *types.MsgCreateProof) (*types.MsgCreateProofResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	if err := k.CreateNewProof(ctx, msg.Hash, msg.Reporter); err != nil {
+		return nil, err
+	}
+
+	return &types.MsgCreateProofResponse{}, nil
+}
