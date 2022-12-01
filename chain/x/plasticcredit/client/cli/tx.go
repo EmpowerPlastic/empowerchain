2090a78a2ea4691868b5405e5a5743811b9f947f
ba1ciu <jakub@empower.eco>
2022-12-01 12:10:18 +0100

    Message validation + cli tx

--- chain/x/plasticcredit/client/cli/tx.go
+++ chain/x/plasticcredit/client/cli/tx.go
@@ -1,10 +1,12 @@
 package cli
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/spf13/cast"
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -23,6 +25,137 @@ func GetTxCmd() *cobra.Command {
 	}
 
 	cmd.AddCommand(MsgCreateApplicantCmd())
+	cmd.AddCommand(MsgIssueCreditsCmd())
+	cmd.AddCommand(MsgTransferCreditsCmd())
+	cmd.AddCommand(MsgRetireCreditsCmd())
+
+	return cmd
+}
+
+func MsgIssueCreditsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "issue-credits [project-id] [denom-suffix] [amount] [credit-data...]",
+		Short: "Issue credits.",
+		Long:  `Issue credits.`,
+		Args:  cobra.MinimumNArgs(4),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			creator := clientCtx.GetFromAddress()
+			projectId, err := cast.ToUint64E(args[0])
+			if err != nil {
+				return err
+			}
+			denomSuffix := args[1]
+			creditAmount := new(plasticcredit.CreditAmount)
+			err = json.Unmarshal([]byte(args[2]), creditAmount)
+			if err != nil {
+				return err
+			}
+			var creditData []*plasticcredit.ProvenData
+			for i := 3; i < len(args); i++ {
+				data := new(plasticcredit.ProvenData)
+				err = json.Unmarshal([]byte(args[i]), data)
+				if err != nil {
+					return err
+				}
+				creditData = append(creditData, data)
+			}
+
+			msg := plasticcredit.MsgIssueCredits{
+				Creator:      creator.String(),
+				ProjectId:    projectId,
+				DenomSuffix:  denomSuffix,
+				CreditAmount: creditAmount,
+				CreditData:   creditData,
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func MsgTransferCreditsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "transfer [receiver] [denom] [amount] [retire?]",
+		Short: "Transfer credits",
+		Long:  `Transfer credits from one address to another. Retire is optional and is set to false by default`,
+		Args:  cobra.MinimumNArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			from := clientCtx.GetFromAddress()
+			to := args[0]
+			denom := args[1]
+			amount, err := cast.ToUint64E(args[2])
+			if err != nil {
+				return err
+			}
+			retire, err := cast.ToBoolE(args[3])
+			if err != nil {
+				retire = false
+			}
+
+			msg := plasticcredit.MsgTransferCredits{
+				From:   from.String(),
+				To:     to,
+				Denom:  denom,
+				Amount: amount,
+				Retire: retire,
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func MsgRetireCreditsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "retire [denom] [amount]",
+		Short: "Retire credits",
+		Long:  `Retire credits`,
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			owner := clientCtx.GetFromAddress()
+			denom := args[0]
+			amount, err := cast.ToUint64E(args[2])
+			if err != nil {
+				return err
+			}
+
+			msg := plasticcredit.MsgRetireCredits{
+				Owner:  owner.String(),
+				Denom:  denom,
+				Amount: amount,
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd
 }
--- chain/x/plasticcredit/msgs.go
+++ chain/x/plasticcredit/msgs.go
@@ -1,6 +1,8 @@
 package plasticcredit
 
 import (
+	"encoding/hex"
+
 	errors "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -74,4 +76,86 @@ func (m *MsgCreateApplicant) GetSigners() []sdk.AccAddress {
 		panic(err)
 	}
 	return []sdk.AccAddress{admin}
+}
+
+func (m *MsgIssueCredits) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(m.Creator)
+	if err != nil {
+		return errors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	if m.ProjectId == 0 {
+		return errors.Wrap(ErrInvalidProjectId, "invalid project id")
+	}
+	if m.DenomSuffix == "" {
+		return errors.Wrap(ErrInvalidDenomSuffix, "invalid denom suffix")
+	}
+	if m.CreditAmount.Active == 0 && m.CreditAmount.Retired == 0 {
+		return errors.Wrap(ErrInvalidCreditAmount, "invalid credit amount")
+	}
+	for _, data := range m.CreditData {
+		_, err = hex.DecodeString(data.Hash)
+		if err != nil {
+			return errors.Wrapf(ErrInvalidHash, "Invalid hash: %s", data.Hash)
+		}
+		if data.Uri == "" {
+			return errors.Wrap(ErrInvalidUri, "Invalid uri")
+		}
+	}
+	return nil
+}
+
+func (m *MsgIssueCredits) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(m.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (m *MsgTransferCredits) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(m.From)
+	if err != nil {
+		return errors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid sender address (%s)", err)
+	}
+	_, err = sdk.AccAddressFromBech32(m.To)
+	if err != nil {
+		return errors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid recipient address (%s)", err)
+	}
+	if m.Denom == "" {
+		return errors.Wrap(ErrInvalidDenom, "invalid denom")
+	}
+	if m.Amount == 0 {
+		return errors.Wrap(ErrInvalidCreditAmount, "invalid credit amount")
+	}
+	return nil
+}
+
+func (m *MsgTransferCredits) GetSigners() []sdk.AccAddress {
+	from, err := sdk.AccAddressFromBech32(m.From)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{from}
+}
+
+func (m *MsgRetireCredits) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(m.Owner)
+	if err != nil {
+		return errors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid owner address (%s)", err)
+	}
+	if m.Denom == "" {
+		return errors.Wrap(ErrInvalidDenom, "invalid denom")
+	}
+	if m.Amount == 0 {
+		return errors.Wrap(ErrInvalidCreditAmount, "invalid credit amount")
+	}
+	return nil
+}
+
+func (m *MsgRetireCredits) GetSigners() []sdk.AccAddress {
+	owner, err := sdk.AccAddressFromBech32(m.Owner)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{owner}
 }
