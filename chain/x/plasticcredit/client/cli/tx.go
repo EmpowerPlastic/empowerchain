f3e33b03250f21fb40346f1d3de23e0100a8ede7
ba1ciu <jakub@empower.eco>
2022-12-22 21:04:19 +0100

    Lint & format & test timeout increase becasue of e2e tests

--- chain/x/plasticcredit/client/cli/tx.go
+++ chain/x/plasticcredit/client/cli/tx.go
@@ -132,7 +132,6 @@ func MsgUpdateIssuerCmd() *cobra.Command {
 `,
 		Args: cobra.MinimumNArgs(4),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
