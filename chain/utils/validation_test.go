a8f8b10b566e37a51802b8da42a4f0d26099442d
aljo242 <alex@shmeeload.xyz>
2023-02-10 16:15:49 -0500

    util test

--- chain/utils/validation_test.go
+++ chain/utils/validation_test.go
@@ -48,3 +48,42 @@ func TestIsBasicValidName(t *testing.T) {
 		})
 	}
 }
+
+func TestIsValidDescription(t *testing.T) {
+	testCases := map[string]struct {
+		description    string
+		expectedResult bool
+	}{
+		"Happy path 1": {
+			description:    "Perfectly valid description",
+			expectedResult: true,
+		},
+		"Happy path 2:": {
+			description:    "also OK_78%$",
+			expectedResult: true,
+		},
+		"Happy path empty string:": {
+			description:    "",
+			expectedResult: true,
+		},
+		"Happy path full length:": {
+			description:    "aylPbqP5OKRLHZvvt8gMZ1r0D02cJkf4mq7PLY8xOFq1bAQPEHvtH3AY4MqHTnfMHPWgHvhOYpmkndMyRqBvkeDZJjMOPMKtiekk6IaYWLTwHaEXC6723LFtjfCCfMN8LaUxWtsSGSWkyn17LKE3QXZLJ6gGDZe9JKAqsGOhQBF1LN5o9pO3E6hMe7ALBIUMdxSpnElqYzDsqBu4u5eAcswBowKwcSWnYI9U1iXQQjQvXsr3PJoBP83oL5Km1RZv",
+			expectedResult: true,
+		},
+		"Too long name should fail": {
+			description:    "aylPbqP5OKRLHZvvt8gMZ1r0D02cJkf4mq7PLY8xOFq1bAQPEHvtH3AY4MqHTnfMHPWgHvhOYpmkndMyRqBvkeDZJjMOPMKtiekk6IaYWLTwHaEXC6723LFtjfCCfMN8LaUxWtsSGSWkyn17LKE3QXZLJ6gGDZe9JKAqsGOhQBF1LN5o9pO3E6hMe7ALBIUMdxSpnElqYzDsqBu4u5eAcswBowKwcSWnYI9U1iXQQjQvXsr3PJoBP83oL5Km1RZv+",
+			expectedResult: false,
+		},
+		"Non-utf8 characters should fail": {
+			description:    "This name has a non-utf8 character: \x80 and it should fail",
+			expectedResult: false,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result := IsValidDescription(tc.description)
+			require.Equal(t, tc.expectedResult, result)
+		})
+	}
+}
