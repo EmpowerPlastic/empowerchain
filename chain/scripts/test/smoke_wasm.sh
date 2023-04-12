b7cd8ca80f7f0fa0d410f5a1a54122f4064f6176
Gjermund Garaba <gjermund@garaba.net>
2023-04-12 10:27:41 +0200

    Fix wasm smoke test

--- chain/scripts/test/smoke_wasm.sh
+++ chain/scripts/test/smoke_wasm.sh
@@ -14,7 +14,7 @@ STORE_TX_HASH=$($BINARY tx wasm store $WASM_FILE --from alice --home $CHAIN_DIR 
 sleep 5
 CODE_ID=$($BINARY q tx $STORE_TX_HASH --output json | jq -r '.events[] | select(.type=="store_code") | .attributes[] | select(.key=="code_id") | .value')
 sleep 5
-INSTANTIATE_TX_HASH=$($BINARY tx wasm instantiate $CODE_ID '{}' --label marketplace --admin empower18hl5c9xn5dze2g50uaw0l2mr02ew57zkk9vga7 --from alice --home $CHAIN_DIR --node tcp://:$RPC_PORT --keyring-backend test --gas auto --gas-prices 0.025umpwr --gas-adjustment 1.75 --chain-id $CHAIN_ID --yes --output json | jq -r ".txhash")
+INSTANTIATE_TX_HASH=$($BINARY tx wasm instantiate $CODE_ID '{"admin": "empower18hl5c9xn5dze2g50uaw0l2mr02ew57zkk9vga7", "fee_percentage": "0", "shares": []}' --label marketplace --admin empower18hl5c9xn5dze2g50uaw0l2mr02ew57zkk9vga7 --from alice --home $CHAIN_DIR --node tcp://:$RPC_PORT --keyring-backend test --gas auto --gas-prices 0.025umpwr --gas-adjustment 1.75 --chain-id $CHAIN_ID --yes --output json | jq -r ".txhash")
 sleep 5
 CONTRACT_ADDRESS=$($BINARY q tx $INSTANTIATE_TX_HASH --output json | jq -r '.events[] | select(.type=="instantiate") | .attributes[] | select(.key=="_contract_address") | .value')
 sleep 5
