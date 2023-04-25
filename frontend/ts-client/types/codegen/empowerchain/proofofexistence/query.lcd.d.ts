858ad3a7acf1d1fcb8e4f773d0d5e4139c53bda4
Shan <shanpirana@gmail.com>
2023-04-25 10:54:36 +0530

    removed ts-clinet directory

--- frontend/ts-client/types/codegen/empowerchain/proofofexistence/query.lcd.d.ts
+++
@@ -1,9 +0,0 @@
-import { LCDClient } from "@osmonauts/lcd";
-import { QueryProofRequest, QueryProofResponseSDKType } from "./query";
-export declare class LCDQueryClient {
-    req: LCDClient;
-    constructor({ requestClient }: {
-        requestClient: LCDClient;
-    });
-    proof(params: QueryProofRequest): Promise<QueryProofResponseSDKType>;
-}
