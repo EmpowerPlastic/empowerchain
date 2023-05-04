865c611c01884beea95c1b1a23fc7ba63941238c
Shan <shanpirana@gmail.com>
2023-05-05 01:21:24 +0530

    Retire credit modal

--- frontend/marketplace/src/components/RetireCreditsModal.vue
+++ frontend/marketplace/src/components/RetireCreditsModal.vue
@@ -7,9 +7,8 @@ defineProps<ModalProps>();
 </script>
 <template>
   <input type="checkbox" id="retire-credits-modal" class="modal-toggle" :checked="showModal"/>
-  <div class="modal bg-modalBackground">
-    <div
-        class="modal-box text-white bg-black rounded-sm border-[1.5px] border-borderBlack shadow-md px-7 py-5 max-w-5xl relative font-Inter">
+  <div class="modal bg-white">
+    <div class="modal-box text-white bg-black rounded-sm border-[1.5px] border-borderBlack shadow-md px-7 py-5 max-w-4xl relative">
       <label for="retire-credits-modal" class="btn btn-ghost btn-sm btn-circle absolute right-2 top-2">
         <img class="h-7" src="../assets/closeIcon.svg"/>
       </label>
@@ -20,13 +19,13 @@ defineProps<ModalProps>();
         </p>
       </div>
 
-      <div class="grid md:grid-cols-2 gap-5 mb-6 mt-2">
+      <div class="flex justify-between my-5">
         <div class="form-control">
           <label class="label">
             <span class="label-text text-white text-title18">How many Plastic credits do you want to retire?</span>
           </label>
-          <label class="input-group rounded-sm w-fit">
-            <input type="text" placeholder="0.01" class="input text-white text-title24 bg-mediumGray w-[180px]" />
+          <label class="input-group rounded-sm">
+            <input type="text" placeholder="0.01" class="input text-white text-title24 bg-mediumGray" />
             <span class="bg-mediumGray text-title16 text-dropDownText">$MPWR</span>
           </label>
         </div>
@@ -35,31 +34,23 @@ defineProps<ModalProps>();
           <label class="label">
             <span class="label-text text-white text-title18">Available credits in your account</span>
           </label>
-          <label class="input-group rounded-sm w-fit border-dashed border-[1px] border-dashedBorderBlack">
-            <input type="text" placeholder="0.01" class="input text-white text-title24 bg-mediumGray w-[180px]" />
+          <label class="input-group rounded-sm">
+            <input type="text" placeholder="0.01" class="input text-white text-title24 bg-mediumGray" />
             <span class="bg-mediumGray text-title16 text-dropDownText">$MPWR</span>
           </label>
         </div>
       </div>
 
-      <div class="my-4">
+     <div>
+       <p class="text-title18">Provide a name of the entity that should be visible as a retriever</p>
+       <p class="text-title14 text-textGray">(will be visible on generated Plastic Credit certificate)</p>
+       <textarea class="textarea w-full bg-mediumGray rounded-sm" rows="4" ></textarea>
+     </div>
+
+      <div>
         <p class="text-title18">Provide a name of the entity that should be visible as a retriever</p>
         <p class="text-title14 text-textGray">(will be visible on generated Plastic Credit certificate)</p>
-        <textarea class="textarea w-full bg-mediumGray rounded-sm my-1" rows="3"></textarea>
-      </div>
-
-      <div class="my-4">
-        <p class="text-title18">Provide additional data about the retriever</p>
-        <p class="text-title14 text-textGray">(will be visible on generated Plastic Credit certificate)</p>
-        <textarea class="textarea w-full bg-mediumGray  rounded-sm  my-1" rows="3"></textarea>
-      </div>
-
-      <div class="flex flex-col md:flex-row justify-between modal-action">
-        <label for="retire-credits-modal"
-               class="btn rounded-sm  font-bold text-greenPrimary text-title14 bg-lightGray normal-case border-none px-10 mb-4 md:m-0">Cancel!</label>
-        <button class="btn rounded-sm  font-bold text-title14 bg-greenPrimary normal-case border-none px-10 !ml-0">
-          Retire credits
-        </button>
+        <textarea class="textarea w-full bg-mediumGray  rounded-sm" rows="4" ></textarea>
       </div>
 
     </div>
