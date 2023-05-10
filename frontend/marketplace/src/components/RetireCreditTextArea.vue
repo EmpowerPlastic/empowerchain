812156131394a886c3fef9f82a18efee7fde2aed
Shan <shanpirana@gmail.com>
2023-05-10 11:55:06 +0530

    build issue fix

--- frontend/marketplace/src/components/RetireCreditTextArea.vue
+++ frontend/marketplace/src/components/RetireCreditTextArea.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
 export interface RetireCreditsTextAreaProps {
-  modelValue:string
+  modelValue: string
   label: string
   subText: string
 }
-defineProps<RetireCreditsTextAreaProps>();
+defineProps<RetireCreditsTextAreaProps>()
 
-const emitModalValue = defineEmits(["update:modelValue"]);
+const emitModalValue = defineEmits(['update:modelValue'])
+
+const updateValue = (e: Event) => {
+  emitModalValue('update:modelValue', (e.target as HTMLInputElement).value)
+}
 </script>
 <template>
   <div class="my-2">
-    <p class="text-title18">{{label}}</p>
-    <p class="text-title14 text-textGray">{{subText}}</p>
-    <textarea class="textarea w-full bg-mediumGray rounded-sm my-1" rows="3" :value="modelValue" @input="emitModalValue('update:modelValue', $event.target.value)"/>
+    <p class="text-title18">{{ label }}</p>
+    <p class="text-title14 text-textGray">{{ subText }}</p>
+    <textarea
+      class="textarea w-full bg-mediumGray rounded-sm my-1"
+      rows="3"
+      :value="modelValue"
+      @input="updateValue"
+    />
   </div>
 </template>
