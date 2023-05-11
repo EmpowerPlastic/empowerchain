a2aae07fea9c2e9784805fe53a7b99fd6aad11f9
Shan <shanpirana@gmail.com>
2023-05-11 21:47:07 +0530

    pagination added and PR review changes

---
+++ frontend/marketplace/src/components/CustomPagination.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Paginate from 'vuejs-paginate-next'
+
+export interface PaginationProps {
+  pageCount: number
+  currentPage: number
+}
+defineProps<PaginationProps>()
+
+const emitPagination = defineEmits(['update:currentPage', 'pageChange'])
+
+const handlePageChange = (current: number) => {
+  emitPagination('update:currentPage', current)
+  emitPagination('pageChange', current)
+}
+</script>
+<template>
+  <paginate
+    :v-model="currentPage"
+    :page-count="20"
+    :click-handler="handlePageChange"
+    first-last-button
+    prev-text="<"
+    next-text=">"
+    first-button-text="<<"
+    last-button-text=">>"
+    :container-class="'rounded-sm flex bg-lightBlack rounded-sm items-center cursor-pointer'"
+    :page-class="'md:px-4 px-2 py-3 bg-lightBlack border-none text-greenTabs'"
+    :prev-class="'md:px-4 px-2 py-3 bg-lightBlack border-none  text-greenTabs'"
+    :next-class="'md:px-4 px-2 py-3 bg-lightBlack border-none  text-greenTabs'"
+    :active-class="'md:px-4 px-2 py-3 bg-paginationBlack border-none  text-greenTabs'"
+  />
+</template>
--- frontend/marketplace/src/pages/MyCredits.vue
+++ frontend/marketplace/src/pages/MyCredits.vue
@@ -1,39 +1,64 @@
 <script setup lang="ts">
-import CreditsSearchBar from "@/components/CreditsSearchBar.vue";
-import CreditCard from "@/components/CreditCard.vue";
+import CreditsSearchBar from '@/components/CreditsSearchBar.vue'
+import CreditCard from '@/components/CreditCard.vue'
+import { ref } from 'vue'
+import CustomPagination from '@/components/CustomPagination.vue'
 
-const creditCardData = {
-  creditType: "PCRD",
-  material: "PP, PET",
-  price: "100.00"
+const creditArray = [
+  {
+    id: '1',
+    creditType: 'PCRD',
+    material: 'PP, PET',
+    price: '100.00'
+  },
+  {
+    id: '2',
+    creditType: 'PCRD',
+    material: 'PP, PET',
+    price: '100.00'
+  },
+  {
+    id: '3',
+    creditType: 'PCRD',
+    material: 'PP, PET',
+    price: '100.00'
+  },
+  {
+    id: '4',
+    creditType: 'PCRD',
+    material: 'PP, PET',
+    price: '100.00'
+  }
+]
+const pageNumber = ref(1)
+const searchTerm = ref('')
+
+const handlePageChange = (currentPage: number) => {
+  console.log(currentPage, pageNumber.value)
 }
 </script>
 <template>
-
   <div class="p-5 md:px-[10%] min-h-[60vh] text-white font-Inter">
-
     <div
-        class="grid grid-cols-2 gap-4 max-w-[350px] bg-dividerGray w-full items-center rounded-xl border-[1.5px] border-borderGray p-1">
+      class="grid grid-cols-2 gap-4 max-w-[350px] bg-dividerGray w-full items-center rounded-xl border-[1.5px] border-borderGray p-1"
+    >
       <div class="text-center rounded-xl p-2">My certificates</div>
       <div class="text-center bg-greenTabs rounded-xl p-2">My credits</div>
     </div>
 
-    <CreditsSearchBar/>
-    <CreditCard :card-data="creditCardData"/>
-    <CreditCard :card-data="creditCardData"/>
-    <CreditCard :card-data="creditCardData"/>
-    <CreditCard :card-data="creditCardData"/>
+    <CreditsSearchBar v-model="searchTerm" />
 
-    <div class="w-full flex justify-center md:justify-end my-10">
-      <div class="btn-group rounded-sm">
-        <button class="btn bg-lightBlack border-none text-greenTabs text-title16">«</button>
-        <button class="btn bg-paginationBlack border-none">1</button>
-        <button class="btn bg-lightBlack border-none text-greenTabs">2</button>
-        <button class="btn bg-lightBlack border-none text-greenTabs">3</button>
-        <button class="btn bg-lightBlack border-none text-greenTabs">4</button>
-        <button class="btn bg-lightBlack border-none text-greenTabs">»</button>
-      </div>
+    <div v-for="credit in creditArray" :key="credit.id">
+      <CreditCard :card-data="credit" />
     </div>
 
+    <div class="flex justify-center md:justify-end my-10">
+      <CustomPagination
+        :page-count="20"
+        v-model:current-page="pageNumber"
+        @page-change="handlePageChange"
+      />
+    </div>
   </div>
 </template>
+<style scoped></style>
