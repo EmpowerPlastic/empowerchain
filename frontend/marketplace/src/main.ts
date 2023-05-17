1ff577e54c612e8007576094adcf173ad3696097
Jakub Kobeldys <jakub@kobeldys.com>
2023-05-17 12:45:04 +0200

    installed apollo packages, init in main.ts

--- frontend/marketplace/src/main.ts
+++ frontend/marketplace/src/main.ts
@@ -3,11 +3,25 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import VueAwesomePaginate from "vue-awesome-paginate";
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+
+
 
 import "vue-awesome-paginate/dist/style.css";
 import './index.css'
 import './css/custom.css'
 
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+  cache,
+  uri: 'http://51.159.168.89:3000/',
+})
+
+const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient,
+  })
 
 const app = createApp(App)
 
@@ -15,4 +29,5 @@ app.use(createPinia())
 app.use(router as any)
 
 app.use(VueAwesomePaginate)
+app.use(apolloProvider)
 app.mount('#app')
