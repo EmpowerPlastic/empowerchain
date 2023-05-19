a76f63835b9dbe6e0b5704e4376ceb7b8dfb551d
Shan <shanpirana@gmail.com>
2023-05-19 10:36:05 +0530

    Appalo integration and sample query

--- frontend/marketplace/src/main.ts
+++ frontend/marketplace/src/main.ts
@@ -1,33 +1,25 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 import App from './App.vue'
 import router from './router'
 import VueAwesomePaginate from "vue-awesome-paginate";
-import { ApolloClient, InMemoryCache } from '@apollo/client/core'
-import { createApolloProvider } from '@vue/apollo-option'
-
-
+import {ApolloClient, InMemoryCache} from '@apollo/client/core'
+import {DefaultApolloClient} from '@vue/apollo-composable'
 
 import "vue-awesome-paginate/dist/style.css";
 import './index.css'
 import './css/custom.css'
 
 const cache = new InMemoryCache()
-
 const apolloClient = new ApolloClient({
-  cache,
-  uri: 'http://51.159.168.89:3000/',
+    cache,
+    uri: 'http://51.159.168.89:3000/',
 })
-
-const apolloProvider = createApolloProvider({
-    defaultClient: apolloClient,
-  })
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router as any)
-
 app.use(VueAwesomePaginate)
-app.use(apolloProvider)
+app.provide(DefaultApolloClient, apolloClient)
 app.mount('#app')
