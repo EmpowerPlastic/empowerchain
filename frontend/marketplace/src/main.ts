094260cc54561bd9fdd5d7b08c7bfc9d1b462e78
Shan <shanpirana@gmail.com>
2023-05-19 10:36:05 +0530

    Appalo integration and sample query

--- frontend/marketplace/src/main.ts
+++ frontend/marketplace/src/main.ts
@@ -1,8 +1,10 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 import App from './App.vue'
 import router from './router'
 import VueAwesomePaginate from "vue-awesome-paginate";
+import {ApolloClient, InMemoryCache} from '@apollo/client/core'
+import {DefaultApolloClient} from '@vue/apollo-composable'
 import VueDatePicker from '@vuepic/vue-datepicker';
 import { ApolloClient, InMemoryCache } from '@apollo/client/core'
 import { createApolloProvider } from '@vue/apollo-option'
@@ -15,22 +17,17 @@ import './index.css'
 import './css/custom.css'
 
 const cache = new InMemoryCache()
-
 const apolloClient = new ApolloClient({
-  cache,
-  uri: 'http://51.159.168.89:3000/',
+    cache,
+    uri: 'http://51.159.168.89:3000/',
 })
-
-const apolloProvider = createApolloProvider({
-    defaultClient: apolloClient,
-  })
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router as any)
-
 app.use(VueAwesomePaginate)
+app.provide(DefaultApolloClient, apolloClient)
 app.component('VueDatePicker', VueDatePicker);
 app.use(apolloProvider)
 app.mount('#app')
--- frontend/marketplace/src/pages/HomePage.vue
+++ frontend/marketplace/src/pages/HomePage.vue
@@ -2,9 +2,35 @@
 import SearchBar from "@/components/SearchBar.vue";
 import AuctionSection from "@/components/AuctionSection.vue";
 import HomePageHeroBanner from "@/components/HomePageHeroBanner.vue";
+import { useQuery } from '@vue/apollo-composable';
+import gql from 'graphql-tag';
+import {onMounted} from "vue";
+
+const loadData=()=>{
+  const { result } = useQuery(gql`
+      query {
+        eventData(first:5){
+    nodes{
+      latitude
+      longitude
+      amount
+      magnitude
+    }
+  }
+      }
+    `);
+console.log(result)
+}
+
+onMounted(()=>{
+  loadData()
+})
+
 </script>
 
 <template>
+
+
   <div class="p-5 md:px-[10%]">
     <SearchBar/>
     <HomePageHeroBanner/>
