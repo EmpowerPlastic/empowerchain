9d3a7b4265721bc7ba3b96288467b523798bfa60
Shan <shanpirana@gmail.com>
2023-05-25 10:22:13 +0530

    image gallery and google map created

---
+++ frontend/marketplace/src/components/ProjectDetailContent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+export interface ProjectDetailContentProps {
+  label: string;
+  value: string
+}
+defineProps<ProjectDetailContentProps>()
+</script>
+<template>
+  <div>
+    <p class="details-label">{{label}}</p>
+    <p class="details-content">{{value}}</p>
+  </div>
+</template>
