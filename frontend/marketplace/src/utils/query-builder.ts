e9814434b3a044e729234d4ec5b318a2a405dcc1
Jakub Kobeldys <jakub@kobeldys.com>
2023-05-25 13:15:25 +0200

    Query listings builder

--- frontend/marketplace/src/utils/query-builder.ts
+++ frontend/marketplace/src/utils/query-builder.ts
@@ -1,3 +1,167 @@
+export class ListingsQueryBuilder {
+    private pricePerCredit: string;
+    private volume: string;
+    private locations: string;
+    private registrationDate: string;
+    private organizations: string;
+    private creditTypes: string;
+    private textSearch: string;
+    
+    constructor() {
+        this.reset();
+    }
+
+
+    public build(): string {
+        const eventData = this.volume || this.locations || this.registrationDate;
+        const creditData = eventData || this.textSearch || this.organizations;
+        const creditCollection = creditData || this.creditTypes;
+        const query = `
+            {
+                ${this.pricePerCredit}
+                ${creditCollection ?
+                `creditCollection:
+                    {
+                        ${creditData ?
+                        `creditData:
+                            {
+                                some:
+                                    {
+                                        ${eventData ?
+                                        `eventData:
+                                            {
+                                                some:
+                                                    {
+                                                        ${this.volume}
+                                                        ${this.locations}
+                                                        ${this.registrationDate}
+                                                    }
+                                            },` : ''}
+                                        ${this.textSearch}
+                                        ${this.organizations}
+                                    }
+                            }` : ''}
+                        ${this.creditTypes}
+                    }` : ''}
+            }`
+        return `query {
+                    marketplaceListings(filter: ${query}) {
+                        ${this.resultsQuery()}
+                    }
+                }`
+    }
+
+    public reset() {
+        this.pricePerCredit = '';
+        this.volume = '';
+        this.locations = '';
+        this.registrationDate = '';
+        this.organizations = '';
+        this.creditTypes = '';
+        this.textSearch = '';
+    }
+
+    public addPricePerCredit(pricePerCreditFrom?: number, pricePerCreditTo?: number) {
+        if (!pricePerCreditFrom && !pricePerCreditTo) {
+            throw new Error('Price per credit filter requires at least one of pricePerCreditFrom or pricePerCreditTo');
+        }
+        this.pricePerCredit = this.numberRangeFilter('pricePerCreditAmount', pricePerCreditFrom, pricePerCreditTo);
+    }
+
+    public addVolume(volumeFrom?: number, volumeTo?: number) {
+        if (!volumeFrom && !volumeTo) {
+            throw new Error('Volume filter requires at least one of volumeFrom or volumeTo');
+        }
+        this.volume = this.numberRangeFilter('amount', volumeFrom, volumeTo);
+    }
+
+    public addLocations(locations: string[]) {
+        this.locations = this.inStringFilter('country', locations);
+    }
+
+    public addRegistrationDate(registrationDateFrom?: Date, registrationDateTo?: Date) {
+        if (!registrationDateFrom && !registrationDateTo) {
+            throw new Error('Registration date filter requires at least one of registrationDateFrom or registrationDateTo');
+        }
+        this.registrationDate = this.dateRangeFilter('registrationDate', registrationDateFrom, registrationDateTo);
+    }
+
+    public addOrganizations(organizations: string[]) {
+        const orgFilter = this.inStringFilter('name', organizations);
+        this.organizations = `applicantDataByCreditDataId: { some: { ${orgFilter} } },`
+    }
+
+    public addCreditTypes(creditTypes: string[]) {
+        this.creditTypes = this.inStringFilter('creditType', creditTypes);
+    }
+
+    public addTextSearch(textSearch: string) {
+        this.textSearch = this.likeInsensitiveFilter('rawJsonData', textSearch);
+    }
+
+    private inStringFilter(filterName: string, values: string[]): string {
+        values = values.map(value => `"${value}"`);
+        return `${filterName}: { in: [${values}] },`;
+    }
+
+    private likeInsensitiveFilter(filterName: string, value: string): string {
+        return `${filterName}: { likeInsensitive: "%${value}%" },`;
+    }
+
+    private numberRangeFilter(filterName: string, from?: number, to?: number): string {
+        return this.rangeFilter(filterName, from?.toString(), to?.toString());
+    }
+
+    private dateRangeFilter(filterName: string, from?: Date, to?: Date): string {
+        return this.rangeFilter(filterName, `"${from?.toISOString()}"`, `"${to?.toISOString()}"`);
+    }
+    
+    private rangeFilter(filterName: string, from?: string, to?: string): string {
+        let filter = `${filterName}: {`;
+        if (from) {
+            filter += `greaterThanOrEqualTo: ${from},`;
+        }
+        if (to) {
+            filter += `lessThanOrEqualTo: ${to},`;
+        }
+        filter += '},';
+        return filter;
+    }
+
+    private resultsQuery(): string {
+        return `nodes {
+                    amount
+                    denom
+                    pricePerCreditAmount
+                    pricePerCreditDenom
+                    creditCollection{
+                        creditType
+                        creditData{
+                            nodes{
+                                eventData{
+                                    nodes{
+                                        country
+                                        material{
+                                            nodes{
+                                                key
+                                                value
+                                            }
+                                        }
+                                    }
+                                }
+                                applicantDataByCreditDataId{
+                                    nodes{
+                                        name
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }`
+    }
+}
+
+// TODO: delete this monster!
 export function queryListings(
     locations?: string[],
     volumeFrom?: number,
@@ -67,35 +231,7 @@ export function queryListings(
 
     return `query {
       marketplaceListings(filter: ${filter}) {
-        nodes {
-          amount
-          denom
-          pricePerCreditAmount
-          pricePerCreditDenom
-          creditCollection{
-            creditType
-            creditData{
-              nodes{
-                eventData{
-                  nodes{
-                    country
-                    material{
-                      nodes{
-                        key
-                        value
-                      }
-                    }
-                  }
-                }
-                applicantDataByCreditDataId{
-                  nodes{
-                    name
-                  }
-                }
-              }
-            }
-          }
-        }
+
       }
     }`;
 }
