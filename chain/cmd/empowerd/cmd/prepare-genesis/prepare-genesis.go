905e750eca86556d1cd2041ff3745156013371ad
Gjermund Garaba <gjermund@garaba.net>
2023-05-27 15:02:34 +0200

    Rest of genesis config

--- chain/cmd/empowerd/cmd/prepare-genesis/prepare-genesis.go
+++ chain/cmd/empowerd/cmd/prepare-genesis/prepare-genesis.go
@@ -8,21 +8,11 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/genutil"
-
-	genesistools "github.com/EmpowerPlastic/empowerchain/app/genesis-tools"
 )
 
-func ValidateAndGenerateGenesisFile(clientCtx client.Context, genesisState *genesistools.GenesisState, appState map[string]json.RawMessage, genDoc *tmtypes.GenesisDoc, fileName string, mbm module.BasicManager) error {
-	cdc := clientCtx.Codec
-
-	// set right supply
-	genesistools.CalculateSupply(genesisState)
-
-	// run Marshal Genesis
-	appState = genesistools.MarshalGenesis(clientCtx, genesisState, appState)
-
+func ValidateAndGenerateGenesisFile(clientCtx client.Context, appState map[string]json.RawMessage, genDoc *tmtypes.GenesisDoc, fileName string, mbm module.BasicManager) error {
 	// validate genesis state
-	if err := mbm.ValidateGenesis(cdc, clientCtx.TxConfig, appState); err != nil {
+	if err := mbm.ValidateGenesis(clientCtx.Codec, clientCtx.TxConfig, appState); err != nil {
 		return fmt.Errorf("error validating genesis file: %s", err.Error())
 	}
 
