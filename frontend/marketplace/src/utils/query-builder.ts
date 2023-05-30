5025ae0b11deadba238fccce3b4ca4ed68f11452
Shan <shanpirana@gmail.com>
2023-05-30 10:13:43 +0530

    auction page listing and pagination

--- frontend/marketplace/src/utils/query-builder.ts
+++ frontend/marketplace/src/utils/query-builder.ts
@@ -1,12 +1,3 @@
-
-
-const queryBuilder = new ListingsQueryBuilder();
-queryBuilder.addCreditTypes(['PCRD']);
-const query = queryBuilder.build();
-
-
-
-
 export class ListingsQueryBuilder {
     private pricePerCredit: string;
     private volume: string;
@@ -57,7 +48,7 @@ export class ListingsQueryBuilder {
             }`
         return `query {
                     marketplaceListings(filter: ${query}, ${this.first ? `first: ${this.first},` : ''} ${this.offset ? `offset: ${this.offset},` : ''}) {
-                        ${this.resultsQuery()}
+                       totalCount ${this.resultsQuery()}
                     }
                 }`
     }
@@ -162,6 +153,8 @@ export class ListingsQueryBuilder {
                             nodes{
                                 eventData{
                                     nodes{
+                                        magnitude
+                                        amount
                                         country
                                         material{
                                             nodes{
@@ -182,106 +175,106 @@ export class ListingsQueryBuilder {
                 }`
     }
 }
-
-// TODO: delete this monster!
-export function queryListings(
-    locations?: string[],
-    volumeFrom?: number,
-    volumeTo?: number,
-    registrationDateFrom?: Date,
-    registrationDateTo?: Date,
-    organizations?: string[],
-    creditTypes?: string[],
-    pricePerCreditFrom?: string,
-    pricePerCreditTo?: string,
-    textSearch?: string) {
-    let filter = '{';
-    if (pricePerCreditFrom || pricePerCreditTo) {
-        filter += 'pricePerCreditAmount: {';
-        if (pricePerCreditFrom) {
-            filter += `greaterThanOrEqualTo: "${pricePerCreditFrom}",`;
-        }
-        if (pricePerCreditTo) {
-            filter += `lessThanOrEqualTo: "${pricePerCreditTo}",`;
-        }
-        filter += '},';
-    }
-    if (volumeFrom || volumeTo || locations || registrationDateFrom || registrationDateTo || organizations || textSearch) {
-        filter += 'creditCollection: { creditData: { some: {';
-        if (volumeFrom || volumeTo || locations || registrationDateFrom || registrationDateTo) {
-            filter += 'eventData: { some: {';
-            if (volumeFrom || volumeTo) {
-                filter += 'amount: {';
-                if (volumeFrom) {
-                    filter += `greaterThanOrEqualTo: ${volumeFrom},`;
-                }
-                if (volumeTo) {
-                    filter += `lessThanOrEqualTo: ${volumeTo},`;
-                }
-                filter += '},';
-            }
-            if (locations) {
-                locations = locations.map(location => `"${location}"`);
-                filter += `country: { in: [${locations}] },`;
-            }
-            if (registrationDateFrom || registrationDateTo) {
-                filter += 'registrationDate: {';
-                if (registrationDateFrom) {
-                    filter += `greaterThanOrEqualTo: "${registrationDateFrom.toISOString()}",`;
-                }
-                if (registrationDateTo) {
-                    filter += `lessThanOrEqualTo: "${registrationDateTo.toISOString()}",`;
-                }
-                filter += '},';
-            }
-            filter += '}, },';
-        }
-        if (textSearch) {
-            filter += `rawJsonData: { likeInsensitive: "%${textSearch}%" },`;
-        }
-        if (organizations) {
-            organizations = organizations.map(org => `"${org}"`);
-            filter += `applicantDataByCreditDataId: { some: { name: { in: [${organizations}] } } },`;
-        }
-        filter += '},},';
-    }
-    if (creditTypes) {
-        creditTypes = creditTypes.map(creditType => `"${creditType}"`);
-        filter += `creditType: { in: [${creditTypes}] },`;
-    }
-    filter += '} }';
-
-    return `query {
-      marketplaceListings(filter: ${filter}) {
-        nodes {
-            amount
-            denom
-            pricePerCreditAmount
-            pricePerCreditDenom
-            creditCollection{
-              creditType
-              creditData{
-                nodes{
-                  eventData{
-                    nodes{
-                      country
-                      material{
-                        nodes{
-                          key
-                          value
-                        }
-                      }
-                    }
-                  }
-                  applicantDataByCreditDataId{
-                    nodes{
-                      name
-                    }
-                  }
-                }
-              }
-            }
-          }
-      }
-    }`;
-}
+//
+// // TODO: delete this monster!
+// export function queryListings(
+//     locations?: string[],
+//     volumeFrom?: number,
+//     volumeTo?: number,
+//     registrationDateFrom?: Date,
+//     registrationDateTo?: Date,
+//     organizations?: string[],
+//     creditTypes?: string[],
+//     pricePerCreditFrom?: string,
+//     pricePerCreditTo?: string,
+//     textSearch?: string) {
+//     let filter = '{';
+//     if (pricePerCreditFrom || pricePerCreditTo) {
+//         filter += 'pricePerCreditAmount: {';
+//         if (pricePerCreditFrom) {
+//             filter += `greaterThanOrEqualTo: "${pricePerCreditFrom}",`;
+//         }
+//         if (pricePerCreditTo) {
+//             filter += `lessThanOrEqualTo: "${pricePerCreditTo}",`;
+//         }
+//         filter += '},';
+//     }
+//     if (volumeFrom || volumeTo || locations || registrationDateFrom || registrationDateTo || organizations || textSearch) {
+//         filter += 'creditCollection: { creditData: { some: {';
+//         if (volumeFrom || volumeTo || locations || registrationDateFrom || registrationDateTo) {
+//             filter += 'eventData: { some: {';
+//             if (volumeFrom || volumeTo) {
+//                 filter += 'amount: {';
+//                 if (volumeFrom) {
+//                     filter += `greaterThanOrEqualTo: ${volumeFrom},`;
+//                 }
+//                 if (volumeTo) {
+//                     filter += `lessThanOrEqualTo: ${volumeTo},`;
+//                 }
+//                 filter += '},';
+//             }
+//             if (locations) {
+//                 locations = locations.map(location => `"${location}"`);
+//                 filter += `country: { in: [${locations}] },`;
+//             }
+//             if (registrationDateFrom || registrationDateTo) {
+//                 filter += 'registrationDate: {';
+//                 if (registrationDateFrom) {
+//                     filter += `greaterThanOrEqualTo: "${registrationDateFrom.toISOString()}",`;
+//                 }
+//                 if (registrationDateTo) {
+//                     filter += `lessThanOrEqualTo: "${registrationDateTo.toISOString()}",`;
+//                 }
+//                 filter += '},';
+//             }
+//             filter += '}, },';
+//         }
+//         if (textSearch) {
+//             filter += `rawJsonData: { likeInsensitive: "%${textSearch}%" },`;
+//         }
+//         if (organizations) {
+//             organizations = organizations.map(org => `"${org}"`);
+//             filter += `applicantDataByCreditDataId: { some: { name: { in: [${organizations}] } } },`;
+//         }
+//         filter += '},},';
+//     }
+//     if (creditTypes) {
+//         creditTypes = creditTypes.map(creditType => `"${creditType}"`);
+//         filter += `creditType: { in: [${creditTypes}] },`;
+//     }
+//     filter += '} }';
+//
+//     return `query {
+//       marketplaceListings(filter: ${filter}) {
+//         nodes {
+//             amount
+//             denom
+//             pricePerCreditAmount
+//             pricePerCreditDenom
+//             creditCollection{
+//               creditType
+//               creditData{
+//                 nodes{
+//                   eventData{
+//                     nodes{
+//                       country
+//                       material{
+//                         nodes{
+//                           key
+//                           value
+//                         }
+//                       }
+//                     }
+//                   }
+//                   applicantDataByCreditDataId{
+//                     nodes{
+//                       name
+//                     }
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//       }
+//     }`;
+// }
