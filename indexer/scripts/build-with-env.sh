9ff246fec83aa8323ee5324db7a66fa7fa965f6a
Jakub Kobeldys <jakub@kobeldys.com>
2023-06-15 09:20:00 +0200

    os build fix

--- indexer/scripts/build-with-env.sh
+++ indexer/scripts/build-with-env.sh
@@ -5,10 +5,18 @@ set -e
 GOOGLE_MAPS_API_KEY=$(grep GOOGLE_MAPS_API_KEY .env | cut -d '=' -f2 | sed 's/"//g')
 
 # Replace $KEY in src/mappings/mappingHandlers.ts with Google Maps API key
-sed -i '' "s/\$KEY/$GOOGLE_MAPS_API_KEY/g" ./src/mappings/mappingHandlers.ts
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sed -i "s/\$KEY/$GOOGLE_MAPS_API_KEY/g" ./src/mappings/mappingHandlers.ts
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s/\$KEY/$GOOGLE_MAPS_API_KEY/g" ./src/mappings/mappingHandlers.ts
+fi
 
 # Build project
 subql build
 
 # Restore original mappingHandlers.ts
-sed -i '' "s/$GOOGLE_MAPS_API_KEY/\$KEY/g" ./src/mappings/mappingHandlers.ts
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sed -i "s/$GOOGLE_MAPS_API_KEY/\$KEY/g" ./src/mappings/mappingHandlers.ts
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s/$GOOGLE_MAPS_API_KEY/\$KEY/g" ./src/mappings/mappingHandlers.ts
+fi
