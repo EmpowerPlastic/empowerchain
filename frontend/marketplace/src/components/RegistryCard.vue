4f0e3757cbc9c555b2d80f358850dd258ed414d9
Shan <shanpirana@gmail.com>
2023-07-28 17:55:27 +0530

    registry card and page UI

---
+++ frontend/marketplace/src/components/RegistryCard.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import router from "@/router";
+import auctionCard from "@/assets/auctionCard.png";
+
+export interface RegistryCardProps {
+  cardData: {
+    retirererName: string;
+    applicant: string;
+    region: string;
+    amount: number;
+  };
+}
+
+const props = defineProps<RegistryCardProps>();
+</script>
+<template>
+  <div
+    class="bg-auctionBackground md:bg-lightBlack rounded-lg font-Inter text-white my-5 md:p-3 md:grid md:grid-cols-5 min-h-[150px]"
+  >
+    <img
+      class="h-[220px] md:h-[140px] w-[100%] md:w-[250px] col-span-1 rounded-sm"
+      :src="auctionCard"
+    />
+
+    <!--    Details for Mobile UI-->
+    <div class="grid grid-cols-2 p-5 gap-4 md:hidden">
+      <div>
+        <p class="text-title17 font-bold">The Coca-Cola Company</p>
+        <p class="text-title15 font-light">Retirerer Name</p>
+      </div>
+      <div class="text-right">
+        <p class="text-title13 font-bold">Asia</p>
+        <p class="text-title11 font-light">Region</p>
+      </div>
+      <div>
+        <p class="text-title14 font-bold">11,000 kgs</p>
+        <p class="text-title12 font-light">Volume</p>
+      </div>
+      <div>
+        <button
+          class="btn bg-greenPrimary w-full h-full rounded-sm text-title15 px-2 normal-case font-normal"
+          @click="router.push(`/registry/1`)"
+        >
+          View details
+        </button>
+      </div>
+    </div>
+
+    <!--      Details for Desktop UI-->
+    <div
+      class="hidden md:grid grid-cols-4 gap-5 w-full col-span-4 py-5 px-6 ml-2 cursor-pointer"
+      @click="router.push(`/auction/1`)"
+    >
+      <div class="col-span-1 ...">
+        <p class="details-title">Retirerer Name</p>
+        <p>The Coca-Cola Company</p>
+      </div>
+      <div class="col-span-1">
+        <p class="details-title">Region</p>
+        <p>Asia</p>
+      </div>
+      <div class="col-span-1">
+        <p class="details-title">Applicant</p>
+        <p>Plastic Collectors Ltd</p>
+      </div>
+      <div class="col-span-1">
+        <div class="flex w-full h-full justify-center items-center">
+          <p class="text-title32 font-bold">11,000 kgs</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ frontend/marketplace/src/components/RegistrySearchBar.vue
@@ -0,0 +1,144 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import SearchFilterSelect from "@/components/SearchFilterSelect.vue";
+import { useQuery } from "@vue/apollo-composable";
+import gql from "graphql-tag";
+
+export interface RegistrySearchBarProps {
+  filterValues?: {
+    retirerer: string[];
+    applicant: string[];
+    region: string[];
+    searchTerm: string;
+  };
+}
+
+const props = defineProps<RegistrySearchBarProps>();
+const filterValues = ref({
+  retirerer: [],
+  applicant: [],
+  region: [],
+  searchTerm: "",
+});
+
+const emitSearch = defineEmits(["searchClick"]);
+
+const handleSearchButtonClick = () => {
+  emitSearch("searchClick", filterValues.value);
+};
+
+const applicantData: any = useQuery(gql`
+  query {
+    applicantData {
+      nodes {
+        id
+        name
+      }
+    }
+  }
+`);
+</script>
+<template>
+  <!--    Search Auction Mobile-->
+  <div
+    class="md:hidden input-group w-full mb-5 bg-white rounded-full !rounded-lg px-1 border-none font-Inter"
+  >
+    <img class="ml-3" src="../assets/searchIcon.svg" />
+    <input
+      type="text"
+      placeholder="Search…"
+      class="input w-full bg-white border-none"
+      v-model="filterValues.searchTerm"
+    />
+    <div class="dropdown dropdown-end">
+      <button class="btn btn-ghost bg-white !rounded-lg border-none p-0">
+        <img class="mr-4" src="../assets/filterIcon.svg" />
+      </button>
+      <div
+        class="dropdown-content menu p-2 shadow bg-dropdownBlack rounded-lg w-[85vw] p-5 gap-4 font-normal"
+      >
+        <div>
+          <p class="text-title14 text-dropDownText">Retirerer</p>
+          <SearchFilterSelect
+            select
+            :options="Array.from(new Set(locationData?.result.value?.countries?.nodes.map((item: any) => item.id)))"
+            v-model="filterValues.retirerer"
+            placeholder="Select Retirerer"
+          />
+        </div>
+        <div>
+          <p class="filter-subtitle">Applicant</p>
+          <SearchFilterSelect
+            select
+            :options="Array.from(new Set(locationData?.result.value?.countries?.nodes.map((item: any) => item.id)))"
+            v-model="filterValues.applicant"
+            placeholder="Select Applicant"
+          />
+        </div>
+        <div>
+          <p class="filter-subtitle">Region</p>
+          <SearchFilterSelect
+            :options="Array.from(new Set(applicantData?.result.value?.applicantData?.nodes.map((item: any)=>item.name)))"
+            v-model="filterValues.region"
+            placeholder="Select Region"
+          />
+        </div>
+        <div>
+          <button
+            class="btn btn-ghost bg-greenPrimary w-full rounded-sm text-white text-title15 px-2"
+            @click="handleSearchButtonClick"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!--Search and filter Desktop-->
+  <div
+    class="hidden md:grid grid-flow-row auto-rows-max mb-10 font-Inter h-full"
+  >
+    <div class="flex flex-row bg-darkGray w-full p-4 rounded-t-sm">
+      <input
+        class="w-full rounded-sm bg-darkGray px-3 h-12 text-lightGray text-title24"
+        placeholder="Search auctions"
+        v-model="filterValues.searchTerm"
+      />
+      <button
+        class="btn bg-greenPrimary w-[60px] ml-4 rounded-sm"
+        @click="handleSearchButtonClick"
+      >
+        <img class="h-5" src="../assets/searchIconWhite.svg" />
+      </button>
+    </div>
+    <div
+      class="flex divide-x divide-dividerGray bg-mediumGray w-full rounded-b-sm"
+    >
+      <div class="filter-box">
+        <p class="filter-subtitle">Retirerer</p>
+        <SearchFilterSelect
+          :options="Array.from(new Set(applicantData?.result.value?.countries?.nodes.map((item: any)=>item.id)))"
+          v-model="filterValues.retirerer"
+          placeholder="Select Retirerer"
+        />
+      </div>
+      <div class="filter-box">
+        <p class="filter-subtitle">Applicant</p>
+        <SearchFilterSelect
+          :options="Array.from(new Set(applicantData?.result.value?.applicantData?.nodes.map((item: any)=>item.name)))"
+          v-model="filterValues.applicant"
+          placeholder="Select Applicant"
+        />
+      </div>
+      <div class="filter-box">
+        <p class="filter-subtitle">Regions</p>
+        <SearchFilterSelect
+          :options="Array.from(new Set(applicantData?.result.value?.applicantData?.nodes.map((item: any)=>item.name)))"
+          v-model="filterValues.region"
+          placeholder="Select Region"
+        />
+      </div>
+    </div>
+  </div>
+</template>
