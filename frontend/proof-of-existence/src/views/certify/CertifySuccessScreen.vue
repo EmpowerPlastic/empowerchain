a969ba3f1c8358c6b99bbec8838286bc20dcc894
Shan <shanpirana@gmail.com>
2023-08-10 22:25:32 +0530

    Verify success page and modal

--- frontend/proof-of-existence/src/views/certify/CertifySuccessScreen.vue
+++ frontend/proof-of-existence/src/views/certify/CertifySuccessScreen.vue
@@ -1,56 +1,30 @@
 <script setup lang="ts">
-import { toast } from "vue3-toastify";
+import CertifyView from "@/views/certify/CertifyView.vue";
+import SuccessModal from "@/views/certify/SuccessModal.vue";
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
 
-const copyLink = () => {
-  const url = new URL(window.location.href);
-  url.searchParams.delete("fileName");
-  url.searchParams.delete("time");
+const showSuccessModal = ref(false);
+const route = useRoute();
 
-  url.pathname = "/verify/success";
+onMounted(() => {
+  if (route.query.hash && route.query.time) {
+    showSuccessModal.value = true;
+  }
+});
 
-  navigator.clipboard.writeText(url.toString());
-  toast.info("Link copied to clipboard");
+const closSuccessModal = () => {
+  showSuccessModal.value = false;
 };
 </script>
 
 <template>
-  <div
-    class="flex flex-col items-center w-full px-14 text-left bg-lightBlack rounded-lg md:p-8 md:-mt-[120px]"
-  >
-    <h5 class="mb-2 mt-3 mb-4 font-bold text-white text-title28">
-      Congratulations!
-    </h5>
-    <p class="text-white text-title12 text-center mb-7">
-      This document’s digest is embedded in the EmpowerChain blockchain. It is
-      permanently certified and proven to exist since the transaction was
-      confirmed.
-    </p>
-    <div
-      class="flex justify-center bg-lightGray rounded-lg break-words text-center items-center flex flex-col p-4 mb-5"
-    >
-      <img class="w-20 mb-4" src="../../assets/images/certificate.png" />
-      <div v-show="$route.query.fileName">
-        <p class="text-lightGreen text-title14 break-all">File</p>
-        <p class="text-white text-title18 mb-3 break-all">
-          {{ $route.query.fileName }}
-        </p>
-      </div>
-      <p class="text-lightGreen text-title14" break-all>Certification Time</p>
-      <p class="text-white text-title18 mb-3 break-all">
-        {{ new Date(Number($route.query.time)).toLocaleString() }}
-      </p>
-      <p class="text-lightGreen text-title14 break-all">Data Hash</p>
-      <p class="text-white text-title18 mb-6 break-all">
-        {{ $route.query.hash }}
-      </p>
-    </div>
-    <button
-      @click="copyLink"
-      class="bg-lightGreen mt-7 content-center p-1 px-7 rounded text-white"
-    >
-      Copy verification link
-    </button>
-  </div>
+  <SuccessModal
+    modal-type="CERTIFY"
+    :close-modal="closSuccessModal"
+    v-show="showSuccessModal"
+    :time="new Date(Number(route.query.time)).toLocaleString()"
+    :hash="route.query.hash.toString()"
+  />
+  <CertifyView />
 </template>
-
-<style></style>
--- frontend/proof-of-existence/src/views/certify/CreateProofView.vue
+++ frontend/proof-of-existence/src/views/certify/CreateProofView.vue
@@ -13,7 +13,6 @@ import type { OfflineSigner } from "@cosmjs/proto-signing";
 import type { DeliverTxResponse } from "@cosmjs/stargate/build/stargateclient";
 import { toast } from "vue3-toastify";
 import { getWalletFromType } from "@/utils/wallet-utils";
-import CertifySuccessModal from "@/views/certify/CertifySuccessModal.vue";
 
 const fee = {
   amount: [{ amount: "100000", denom: "umpwr" }],
@@ -26,7 +25,7 @@ const hash: string | string[] = route.params.hash;
 const { createProof } =
   empowerchain.proofofexistence.MessageComposer.withTypeUrl;
 
-const showModal = ref(false);
+const showWalletModal = ref(false);
 const selectedWallet = ref("");
 const errorMessage = ref<string | undefined>();
 const loading = ref(false);
@@ -34,24 +33,18 @@ const address = ref();
 const addressVisible = ref();
 
 //Methods
-const back = () => {
-  router.push({
-    path: `/`,
-  });
-};
-
 const openModal = () => {
-  showModal.value = true;
+  showWalletModal.value = true;
 };
 
-const closeModal = () => {
-  showModal.value = false;
+const closeWalletModal = () => {
+  showWalletModal.value = false;
 };
 
 const handleSelectedWallet = (wallet: Wallet) => {
   selectedWallet.value = wallet;
   showWalletAddress(wallet);
-  closeModal();
+  closeWalletModal();
 };
 
 const showWalletAddress = async (selectedWallet: Wallet) => {
@@ -149,11 +142,10 @@ const copyAddress = async () => {
 </script>
 <template>
   <SelectWalletModel
-    v-show="showModal"
-    :close-modal="closeModal"
+    v-show="showWalletModal"
+    :close-modal="closeWalletModal"
     @selected-wallet="handleSelectedWallet"
   />
-  <CertifySuccessModal close-modal="" />
   <h5
     class="hidden md:block mt-5 my-5 text-title28 text-left font-bold text-textPrimary"
   >
@@ -273,100 +265,6 @@ const copyAddress = async () => {
       </button>
     </div>
   </div>
-  <!--  <div class="w-full text-left">-->
-  <!--      <h5 class="mb-2 mt-3 font-bold text-center text-textPrimary text-title28">-->
-  <!--        Create proof-->
-  <!--      </h5>-->
-  <!--    <div class="grid grid-cols-1 md:grid-cols-3 md:gap-6">-->
-  <!--      <div class="col-span-2 bg-bgPrimary w-full mt-3 custom-shadow rounded-lg">-->
-  <!--        <div-->
-  <!--          class="bg-lightGray rounded-lg break-words text-center items-center flex flex-col p-4"-->
-  <!--        >-->
-  <!--          <img class="w-20 mb-4" src="../../assets/images/certificate.svg" />-->
-  <!--            <div v-show="$route.query.fileName">-->
-  <!--              <p class="text-textPrimary text-title18 mb-3 break-all">-->
-  <!--                {{ $route.query.fileName }}-->
-  <!--              </p>-->
-  <!--            </div>-->
-
-  <!--            <p class="text-textPrimary text-title14 font-bold">Received Time</p>-->
-  <!--            <p class="text-textPrimary text-title18 mb-3 break-all">-->
-  <!--              {{ new Date(Number($route.query.time)).toLocaleString() }}-->
-  <!--            </p>-->
-  <!--            <p class="text-textPrimary text-title14 break-all font-bold">-->
-  <!--              Transaction Hash-->
-  <!--            </p>-->
-  <!--            <p class="text-textPrimary text-title18 mb-6 break-all">-->
-  <!--              {{ $route.params.hash }}-->
-  <!--            </p>-->
-  <!--            <p class="text-textPrimary text-title14">-->
-  <!--              Hooray!! We are just one step away!-->
-  <!--            </p>-->
-  <!--              <p class="text-textPrimary text-title12">-->
-  <!--                Connect your wallet for the transaction fee, its Only 0.00025 $MPWR-->
-  <!--              </p>-->
-  <!--        </div>-->
-  <!--        <div-->
-  <!--          class="p-4 my-4 w-full text-sm text-red-800 rounded-lg bg-red-50 break-all"-->
-  <!--          role="alert"-->
-  <!--          v-show="errorMessage"-->
-  <!--        >-->
-  <!--          {{ errorMessage }}-->
-  <!--        </div>-->
-  <!--        <div class="flex flex-col md:flex-row justify-between">-->
-  <!--            <button-->
-  <!--              :disabled="!selectedWallet || loading"-->
-  <!--              @click="handleTransaction"-->
-  <!--              class="bg-lightGreen disabled:bg-lightGray mt-4 content-center p-1 px-14 rounded text-white"-->
-  <!--            >-->
-  <!--              {{ loading ? "Loading..." : "Complete" }}-->
-  <!--            </button>-->
-  <!--          <button-->
-  <!--            @click="back"-->
-  <!--            class="bg-transparent mt-4 content-center p-1 rounded text-textPrimary"-->
-  <!--          >-->
-  <!--            Back-->
-  <!--          </button>-->
-  <!--        </div>-->
-  <!--      </div>-->
-  <!--      <div class="md:col-span-1 p-3 bg-bgPrimary mt-5 custom-shadow rounded-lg">-->
-  <!--        <h5-->
-  <!--          class="mb-2 mt-3 mb-6 text-2xl font-bold text-textPrimary text-title28"-->
-  <!--        >-->
-  <!--          Wallet-->
-  <!--        </h5>-->
-  <!--              <img-->
-  <!--                src="../../assets/images/wallet-keplr.png"-->
-  <!--                class="h-20 cursor-pointer mb-3"-->
-  <!--                v-if="selectedWallet === Wallet.Keplr"-->
-  <!--              />-->
-  <!--              <img-->
-  <!--                src="../../assets/images/wallet-cosmostation.png"-->
-  <!--                class="h-20 cursor-pointer mb-3"-->
-  <!--                v-if="selectedWallet === Wallet.Cosmostation"-->
-  <!--              />-->
-  <!--              <img-->
-  <!--                src="../../assets/images/wallet-leap.png"-->
-  <!--                class="h-20 cursor-pointer mb-3"-->
-  <!--                v-if="selectedWallet === Wallet.Leap"-->
-  <!--              />-->
-  <!--          <div-->
-  <!--            v-if="addressVisible"-->
-  <!--            class="flex flex-row bg-lightGray rounded-lg p-2 cursor-pointer w-fit mt-5 flex-wrap"-->
-  <!--            @click="copyAddress"-->
-  <!--          >-->
-  <!--            <p class="text-textPrimary text-title12">{{ addressVisible }}</p>-->
-  <!--            <img class="w-4 ml-4" src="../../assets/images/copy-icon.svg" />-->
-  <!--          </div>-->
-  <!--          <button-->
-  <!--            @click="openModal"-->
-  <!--            class="bg-lightGreen mt-7 content-center p-1 px-7 rounded text-white"-->
-  <!--          >-->
-  <!--            {{ selectedWallet ? "Change wallet" : "Connect your wallet" }}-->
-  <!--          </button>-->
-  <!--      </div>-->
-  <!--    </div>-->
-  <!--  </div>-->
 </template>
 
 <style></style>
