0cd8267342c7488a50bb275fb27c7a42ba69170e
Johannes Schill <johannes.schill@gmail.com>
2023-12-01 07:06:46 +0100

    feat(marketplace): Update fetchRepeater to support a 'next' callback function

--- frontend/marketplace/src/utils/fetchRepeater.ts
+++ frontend/marketplace/src/utils/fetchRepeater.ts
@@ -1,12 +1,12 @@
-import { useFetcher } from './fetcher';
+import { useFetcher } from "./fetcher";
 
-async function fetchRepeater(
+export async function fetchRepeater(
   url: string,
+  next: (responseText: string | undefined) => boolean, // TODO: See what api returns and update this logic
   maxAttempts: number = 5,
   timeout: number = 5000,
-  expectedResponse: string, // TODO: See what api returns and update this logic
-  options?: RequestInit,
-): Promise<Response> {
+  options?: RequestInit
+): Promise<Response | null> {
   const { get: getRequest } = useFetcher();
   const controller = new AbortController();
   const signal = controller.signal;
@@ -14,18 +14,23 @@ async function fetchRepeater(
   let response: Response | null = null;
   let attempts = 0;
 
-  while (attempts < maxAttempts && (response === null || response.status !== 200 || !(await response.text()).includes(expectedResponse))) {
-    if (attempts > 0 && !signal.aborted) {
-      controller.abort()
+  const shouldRepeaterAbort = async (response: Response | null) => {
+    const responseText = await response?.text();
+    return responseText !== null && next(responseText);
+  };
+
+  while (attempts < maxAttempts && !(await shouldRepeaterAbort(response))) {
+    attempts++;
+    response = await getRequest(url, { ...(options ?? {}), signal });
+    await new Promise((resolve) => setTimeout(resolve, timeout));
+    if (!signal.aborted) {
+      controller.abort();
     }
-    attempts++
-    response = await getRequest(url, { ...(options ?? {}), signal })
-    await new Promise((resolve) => setTimeout(resolve, timeout))
   }
 
-  if (response === null || response.status !== 200 || !(await response.text()).includes(expectedResponse)) {
-    throw new Error(`Failed to fetch the expected response within ${maxAttempts} attempts`)
-  }
+  // if (response === null || response.status !== 200 || !(await response.text()).includes(expectedResponse)) {
+  //   throw new Error(`Failed to fetch the expected response within ${maxAttempts} attempts`)
+  // }
 
   return response;
 }
