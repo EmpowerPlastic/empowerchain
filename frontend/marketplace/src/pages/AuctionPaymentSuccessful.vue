700505d75297d09533c51fb4d61f426e5011777b
Johannes Schill <johannes.schill@gmail.com>
2023-12-04 20:12:59 +0100

    chore(marketplace): Refactor AuctionPaymentSuccessful to avoid code repetition

--- frontend/marketplace/src/pages/AuctionPaymentSuccessful.vue
+++ frontend/marketplace/src/pages/AuctionPaymentSuccessful.vue
@@ -88,48 +88,30 @@ onMounted(async () => {
     </div>
     <div v-else class="flex justify-center">
       <div
-        v-if="auctionStatus === PaymentStatus.COMPLETE"
         class="bg-white/10 rounded-lg md:rounded-sm p-4 max-w-xl text-center"
       >
-        <span class="font-Inter text-white text-title18">
+        <span
+          v-if="auctionStatus === PaymentStatus.COMPLETE"
+          class="font-Inter text-white text-title18"
+        >
           Your plastic credit purchase was successful and Plastic Credit Offset
           Certificate has been generated for you.
         </span>
-
-        <a
-          class="mt-5 text-white btn btn-ghost btn-block normal-case bg-greenPrimary hover:bg-greenDark text-title24 lg:text-title32 lg:btn-lg p-0 px-12 font-normal md:max-w-lg"
-          href="/certificate"
-          >See your plastic credits</a
+        <span
+          v-else-if="auctionStatus === PaymentStatus.ERROR"
+          class="font-Inter text-white text-title18"
         >
-      </div>
-      <div
-        v-else-if="auctionStatus === PaymentStatus.ERROR"
-        class="bg-white/10 rounded-lg md:rounded-sm p-4 max-w-xl text-center"
-      >
-        <span class="font-Inter text-white text-title18">
           Transaction couldn't be completed. You will receive a refund shortly.
           Please contact us at
           <a href="mailto:marketplace@empower.eco">marketplace@empower.eco</a>
           if you encounter any issues.
         </span>
-
-        <a
-          class="mt-5 text-white btn btn-ghost btn-block normal-case bg-greenPrimary hover:bg-greenDark text-title24 lg:text-title32 lg:btn-lg p-0 px-12 font-normal md:max-w-lg"
-          href="/certificate"
-          >See your plastic credits</a
-        >
-      </div>
-      <div
-        v-else
-        class="bg-white/10 rounded-lg md:rounded-sm p-4 max-w-xl text-center"
-      >
-        <span class="font-Inter text-white text-title18">
+        <span v-else class="font-Inter text-white text-title18">
           Something went wrong with a credit transfer and we're currently
           investigating it. We'll contact you within 2 business days. If you
           haven't heard from us, please send us a message at
           <a href="mailto:marketplace@empower.eco">marketplace@empower.eco</a>.
         </span>
-
         <a
           class="mt-5 text-white btn btn-ghost btn-block normal-case bg-greenPrimary hover:bg-greenDark text-title24 lg:text-title32 lg:btn-lg p-0 px-12 font-normal md:max-w-lg"
           href="/certificate"
