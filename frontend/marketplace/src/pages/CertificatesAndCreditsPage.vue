24ac0ade76dcd49209ef26ab579240fe66acf36e
Johannes Schill <johannes.schill@gmail.com>
2023-12-21 08:31:23 +0100

    fix(marketplace): Remove unused code

--- frontend/marketplace/src/pages/CertificatesAndCreditsPage.vue
+++ frontend/marketplace/src/pages/CertificatesAndCreditsPage.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import CreditTabContent from "@/components/CreditTabContent.vue";
 import CertificateTabContent from "@/components/CertificateTabContent.vue";
 
-const activeTab = ref("MyCertificates");
 </script>
 <template>
   <div class="p-5 md:px-[10%] min-h-[60vh] text-white font-Inter">
