ee0fa31494a65f6f1dbb49270f137cc93bfc9b7a
Johannes Schill <johannes.schill@gmail.com>
2024-02-14 09:18:05 +0100

    fix(marketplace): Show volume and fix alignment on auction cards

--- frontend/marketplace/src/components/AuctionCard.vue
+++ frontend/marketplace/src/components/AuctionCard.vue
@@ -4,19 +4,27 @@ import { convertIPFStoHTTPS } from "@/utils/utils";
 import auctionCard from "@/assets/auctionCard.png";
 import { formatDenom } from "@/utils/wallet-utils";
 import { onMounted, ref } from "vue";
+import { RouterLink } from "vue-router";
+import { PageNames } from "@/router";
 import CustomImage from "@/components/CustomImage.vue";
 import tracking, { TrackEvents } from "@/utils/analytics";
 import CustomSpinner from "./CustomSpinner.vue";
+import { useListingVolume } from "@/utils/useListingVolume";
 export interface AuctionCardProps {
   auctionData: any;
 }
 const props = defineProps<AuctionCardProps>();
 const denom = ref("");
 const showSpinner = ref(true);
+const creditCollection = ref(props.auctionData.creditCollection);
+const { volume } = useListingVolume(creditCollection);
 
 onMounted(async () => {
   denom.value = await formatDenom(props.auctionData?.pricePerCreditDenom);
-  if (props.auctionData?.creditCollection?.creditData?.nodes[0].mediaFiles?.nodes[0].url !== "") {
+  if (
+    props.auctionData?.creditCollection?.creditData?.nodes[0].mediaFiles
+      ?.nodes[0].url !== ""
+  ) {
     showSpinner.value = false;
   }
 });
@@ -30,7 +38,10 @@ const handleViewDetailsClick = () => {
 };
 </script>
 <template>
-  <div class="bg-lightBlack rounded-lg md:rounded-sm">
+  <router-link
+    :to="{ name: PageNames.LISTING, params: { id: props.auctionData.id } }"
+    class="bg-lightBlack rounded-lg md:rounded-sm flex flex-col"
+  >
     <div v-if="showSpinner">
       <CustomSpinner :visible="showSpinner" />
     </div>
@@ -45,48 +56,48 @@ const handleViewDetailsClick = () => {
         "
       />
     </div>
-    <div class="grid grid-cols-2 p-3 gap-4">
+    <div class="grid grid-cols-2 p-3 gap-4 grow">
       <div>
         <div>
           <p
-            class="font-Inter text-white text-title24 md:text-title32 font-bold"
+            class="font-Inter text-white text-title24 md:text-title32 font-bold leading-tight"
           >
             {{ auctionData?.pricePerCreditAmount / 1000000 }} ${{ denom }}
           </p>
-          <p class="font-Inter text-white text-title15 md:text-title18">
+          <p
+            class="font-Inter text-white text-title15 md:text-title18 font-light leading-tight"
+          >
             Price per credit
           </p>
         </div>
       </div>
-      <div>
-        <div class="text-right">
-          <p
-            class="font-Inter text-white text-title13 md:text-title14 font-bold"
-          >
-            <!--            300kg-->
-          </p>
-          <p class="font-Inter text-white text-title11 md:text-title12">
-            <!--            Volume-->
-          </p>
-        </div>
+      <div class="text-right">
+        <p class="font-Inter text-white text-title13 md:text-title14 font-bold">
+          {{ volume }}kg
+        </p>
+        <p
+          class="font-Inter text-white text-title11 md:text-title12 font-light"
+        >
+          Volume
+        </p>
       </div>
-      <div>
-        <div>
-          <p class="font-Inter text-white text-title14 font-bold">
-            {{ auctionData?.amount }}/{{ auctionData?.initialAmount }}
-          </p>
-          <p class="font-Inter text-white text-title12">Available credits</p>
-        </div>
+      <div class="flex flex-col justify-end">
+        <p class="font-Inter text-white text-title14 font-bold">
+          {{ auctionData?.amount }}/{{ auctionData?.initialAmount }}
+        </p>
+        <p class="font-Inter text-white text-title12 font-light">
+          Available credits
+        </p>
       </div>
-      <div>
+      <div class="flex flex-col justify-end">
         <button
           type="button"
-          class="bg-greenPrimary w-full h-full rounded-sm font-Inter text-white md:text-title18 px-2"
+          class="btn bg-greenPrimary border-greenPrimary w-full rounded-sm font-Inter text-white md:text-title18 px-2 max-h-10 hover:bg-greenMedium hover:border-greenMedium font-light"
           @click="handleViewDetailsClick"
         >
           View details
         </button>
       </div>
     </div>
-  </div>
+  </router-link>
 </template>
---
+++ frontend/marketplace/src/utils/useListingVolume.ts
@@ -0,0 +1,29 @@
+import { computed, type Ref, toRefs, type ComputedRef } from "vue";
+import { type CreditCollection } from "@/types/GraphqlSchema";
+
+const isInteger = (value: number) => {
+  console.log("value", value, Math.round(value), Math.round(value) === value);
+  return value == Math.round(value);
+};
+
+export const useListingVolume = (
+  creditCollection: Ref<CreditCollection>,
+): { volume: ComputedRef<number> } => {
+  const { retiredAmount, activeAmount } = toRefs(creditCollection.value);
+  // TODO: Wont work on float numbers
+  if (!isInteger(retiredAmount.value) || !isInteger(activeAmount.value)) {
+    throw new Error(
+      "Listing volume is not a valid number. " + retiredAmount.value,
+    );
+  }
+  const volume = computed(() => {
+    return (
+      parseInt(retiredAmount.value as unknown as string) +
+      parseInt(activeAmount.value as unknown as string)
+    );
+  });
+
+  return {
+    volume,
+  };
+};
