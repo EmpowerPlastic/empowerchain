173f30206b313afd37b64906da158c8ec976fcfd
Johannes Schill <johannes.schill@gmail.com>
2024-02-21 18:57:16 +0100

    fix(marketplace): Links to faq questions (#1185)
    * fix(marketplace): Links to faq questions
    
    * fix(marketplace): Fix scrolling on FAQ

--- frontend/marketplace/src/components/FAQCollapse.vue
+++ frontend/marketplace/src/components/FAQCollapse.vue
@@ -1,15 +1,62 @@
 <script setup lang="ts">
+import { onMounted, ref, computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
 export interface FAQCollapseProps {
+  id: string;
   title: string;
   content: string;
+  name: string;
 }
-defineProps<FAQCollapseProps>();
+const props = defineProps<FAQCollapseProps>();
+const rootElRef = ref<HTMLElement | null>(null);
+const route = useRoute();
+const router = useRouter();
+const isChecked = computed(() => {
+  const { hash } = route;
+  return hash === `#${props.id}`;
+});
+
+onMounted(() => {
+  const { hash } = route;
+  if (hash === `#${props.id}` && rootElRef.value) {
+    const topPos = rootElRef.value.getBoundingClientRect().top + window.scrollY;
+    // need to push it back to the next tick to ensure the element is in the DOM
+    setTimeout(() => {
+      window.scrollTo({
+        top: topPos, // scroll so that the element is at the top of the view
+        behavior: "smooth", // smooth scroll
+      });
+    }, 1);
+  }
+});
+
+const handleClick = () => {
+  if (isChecked.value) {
+    router.push({ hash: "" });
+  } else {
+    router.push({ hash: `#${props.id}` });
+  }
+};
 </script>
 <template>
-  <div tabindex="0" class="collapse group collapse-arrow mb-1">
-    <div class="flex collapse-title text-white bg-mediumGray">
+  <div
+    :id="props.id"
+    ref="rootElRef"
+    class="collapse group collapse-arrow mb-1"
+  >
+    <input
+      type="radio"
+      :name="props.name"
+      :checked="isChecked"
+      :onClick="handleClick"
+    />
+    <div
+      class="flex collapse-title text-white bg-mediumGray"
+      :onClick="handleClick"
+    >
       <div
-        class="w-7 h-7 border-2 border-white rounded-full mr-3 group-focus:bg-greenPrimary"
+        class="w-7 h-7 border-2 border-white rounded-full mr-3"
+        :class="{ 'bg-greenPrimary': isChecked }"
       />
       {{ title }}
     </div>
